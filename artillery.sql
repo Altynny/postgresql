122ed3ccf1b342b958507d79364b1ae8537b6fa7
Altynny <159096393+Altynny@users.noreply.github.com>
2025-02-14 09:14:59 +0800

    12.02.25_update

--- artillery.sql
+++ artillery.sql
@@ -1,3 +1,56 @@
+do $$
+begin
+/*
+Скрипт создания информационной базы данных
+Согласно технического задания https://git.hostfl.ru/VolovikovAlex/Study2025
+Редакция 2025-02-12
+Edit by valex
+*/
+
+
+/*
+ 1. Удаляем старые элементы
+ ======================================
+ */
+
+raise notice 'Запускаем создание новой структуры базы данных meteo'; 
+begin
+
+	-- Связи
+	alter table if exists public.measurment_input_params
+	drop constraint if exists measurment_type_id_fk;
+
+	alter table if exists public.employees
+	drop constraint if exists military_rank_id_fk;
+
+	alter table if exists public.measurment_baths
+	drop constraint if exists measurment_input_param_id_fk;
+
+	alter table if exists public.measurment_baths
+	drop constraint if exists emploee_id_fk;
+
+	-- Таблицы
+	drop table if exists public.measurment_input_params;
+	drop table if exists public.measurment_baths;
+	drop table if exists public.employees;
+	drop table if exists public.measurment_types;
+	drop table if exists public.military_ranks;
+
+	-- Нумераторы
+	drop sequence if exists public.measurment_input_params_seq;
+	drop sequence if exists public.measurment_baths_seq;
+	drop sequence if exists public.employees_seq;
+	drop sequence if exists public.military_ranks_seq;
+	drop sequence if exists public.measurment_types_seq;
+end;
+
+raise notice 'Удаление старых данных выполнено успешно';
+
+/*
+ 2. Добавляем структуры данных 
+ ================================================
+ */
+
 -- Справочник должностей
 create table military_ranks
 (
@@ -11,7 +64,6 @@ values(1,'Рядовой'),(2,'Лейтенант');
 create sequence military_ranks_seq start 3;
 
 alter table military_ranks alter column id set default nextval('public.military_ranks_seq');
-select * from military_ranks;
 
 -- Пользователя
 create table employees
@@ -28,7 +80,7 @@ values(1, 'Воловиков Александр Сергеевич','1978-06-24
 create sequence employees_seq start 2;
 
 alter table employees alter column id set default nextval('public.employees_seq');
-select * from employees;
+
 
 -- Устройства для измерения
 create table measurment_types
@@ -45,8 +97,6 @@ values(1, 'ДМК', 'Десантный метео комплекс'),
 create sequence measurment_types_seq start 3;
 
 alter table measurment_types alter column id set default nextval('public.measurment_types_seq');
-
-select * from measurment_types;
 
 -- Таблица с параметрами
 create table measurment_input_params
@@ -66,7 +116,6 @@ values(1, 1, 100,12,34,0.2,45);
 create sequence measurment_input_params_seq start 2;
 
 alter table measurment_input_params alter column id set default nextval('public.measurment_input_params_seq');
-select * from measurment_input_params;
 
 -- Таблица с историей
 create table measurment_baths
@@ -85,16 +134,65 @@ create sequence measurment_baths_seq start 2;
 
 alter table measurment_baths alter column id set default nextval('public.measurment_baths_seq');
 
-select * from measurment_baths;
+raise notice 'Создание общих справочников и наполнение выполнено успешно'; 
 
--- Готово
+/*
+ 3. Подготовка расчетных структур
+ ==========================================
+ */
+
+drop table if exists calc_temperatures_correction;
+create table calc_temperatures_correction
+(
+   temperature numeric(8,2) primary key,
+   correction numeric(8,2)
+);
+
+insert into public.calc_temperatures_correction(temperature, correction)
+Values(0, 0.5),(5, 0.5),(10, 1), (20,1), (25, 2), (30, 3.5), (40, 4.5);
+
+drop type  if exists interpolation_type;
+create type interpolation_type as
+(
+	x0 numeric(8,2),
+	x1 numeric(8,2),
+	y0 numeric(8,2),
+	y1 numeric(8,2)
+);
+
+raise notice 'Расчетные структуры сформированы';
+
+/*
+ 4. Создание связей
+ ==========================================
+ */
+
+begin 
+	
+	alter table public.measurment_baths
+	add constraint emploee_id_fk 
+	foreign key (emploee_id)
+	references public.employees (id);
+	
+	alter table public.measurment_baths
+	add constraint measurment_input_param_id_fk 
+	foreign key(measurment_input_param_id)
+	references public.measurment_input_params(id);
+	
+	alter table public.measurment_input_params
+	add constraint measurment_type_id_fk
+	foreign key(measurment_type_id)
+	references public.measurment_types (id);
+	
+	alter table public.employees
+	add constraint military_rank_id_fk
+	foreign key(military_rank_id)
+	references public.military_ranks (id);
+
+end;
+
+raise notice 'Связи сформированы';
+raise notice 'Структура сформирована успешно';
 
 
-
-
-
-
-
-
-
-
+end $$;
